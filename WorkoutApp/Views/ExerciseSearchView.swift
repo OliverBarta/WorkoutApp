a3ca82f5749c2f884c5825c545830681084693d2
Joshua Lin <joshualin40@gmail.com>
2026-07-18 00:29:25 -0400

    Added basic exercise search

--- WorkoutApp/Views/ExerciseSearchView.swift
+++ WorkoutApp/Views/ExerciseSearchView.swift
@@ -1,15 +1,47 @@
-////
-////  ExerciseSearchView.swift
-////  WorkoutApp
-////
-////  Created by Joshua Lin on 2026-07-17.
-////
+//  ExerciseSearchView.swift
+//  WorkoutApp
 //
-//import SwiftUI
+//  Created by Joshua Lin on 2026-07-17.
 //
-//struct ExerciseSearchView: View {
-//    
-//}
-//#Preview {
-//    HomeView()
-//}
+
+import SwiftUI
+
+struct ExerciseSearchView: View {
+    @State private var searchText = ""
+    
+    var filtered: [ExerciseTemplate] {
+        if searchText.isEmpty {
+            return ExerciseCatalog.all
+        }
+        return ExerciseCatalog.all.filter {
+            $0.name.localizedCaseInsensitiveContains(searchText)
+        }
+    }
+    
+    var body: some View {
+          NavigationStack {
+              List(filtered, id: \.name) { exercise in
+                  HStack {
+                      Image(exercise.imageName)
+                          .resizable()
+                          .scaledToFit()
+                          .frame(width: 50, height: 50)
+                      
+                      VStack(alignment: .leading) {
+                          Text(exercise.name)
+                              .font(.headline)
+                          Text(exercise.muscleGroup)
+                              .font(.subheadline)
+                              .foregroundStyle(.secondary)
+                      }
+                  }
+              }
+              .navigationTitle("Exercises")
+              .searchable(text: $searchText, prompt: "Search exercises")
+          }
+      }
+  }
+
+#Preview {
+    ExerciseSearchView()
+}
